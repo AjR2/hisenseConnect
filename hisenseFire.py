e150deab568094df6f10da7180f28dd97cf85169
AJ <139691145+AjR2@users.noreply.github.com>
2024-08-02 21:21:01 -0400

    Update hisenseFire.py

--- hisenseFire.py
+++ hisenseFire.py
@@ -3,8 +3,8 @@ from adb_shell.adb_device import AdbDeviceTcp
 from adb_shell.auth.sign_pythonrsa import PythonRSASigner
 
 # Replace with your Fire TV's IP address and port
-FIRE_TV_IP = "192.100.10.100"  # Change this to your TV's IP address
-FIRE_TV_PORT = 5555            #Default port number
+FIRE_TV_IP = "{"IP")  # Change this to your TV's IP address
+FIRE_TV_PORT = {"port}            #Default port number
 
 # Path to your private key for ADB authentication
 PRIVATE_KEY_PATH = "/Users/ajrudd/.android/adbkey"  # Change this to your adbkey file path
